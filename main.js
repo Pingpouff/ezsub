f6dc0201bb25d52c18229a0cd27942ed9be729ba
pingpouff <pingpouff@gmail.com>
2016-01-21 22:14:42 +0100

    Added absolute path management

--- main.js
+++ main.js
@@ -3,25 +3,24 @@ var fs = require("fs");
 var path = require("path");
 
 var subdb = new SubDb();
-
-console.log("Hashing file at: "+path_to_movie);
-subdb.computeHash(path_to_movie, function(err, res) {
-    if(err)
-    {
-      console.log(err);
-      return err;
+var path_to_movie = process.argv[2];
+console.log("Hashing file at: " + path_to_movie);
+subdb.computeHash(path_to_movie, function (err, res) {
+    if (err) {
+        console.log(err);
+        return err;
     }
     console.log("Hash done.");
     var hash = res;
     console.log("Searching...");
-    subdb.api.search_subtitles(hash, function(err, res){
+    subdb.api.search_subtitles(hash, function (err, res) {
 
-        if(err) return err;
+        if (err) return err;
         console.log("Search done.");
-        var fileName = path.basename(path_to_movie, path.extname(path_to_movie));
-        console.log("Downloading file for: "+fileName);
-        subdb.api.download_subtitle(hash, res.join(','), fileName+'.srt', function(err, res) {
-            if(err) return err;
+        var subFileName = path.dirname(path.resolve(path_to_movie)) + "/" + path.basename(path_to_movie, path.extname(path_to_movie)) + '.srt';
+        console.log("Downloading file for: " + subFileName);
+        subdb.api.download_subtitle(hash, res.join(','), subFileName, function (err, res) {
+            if (err) return err;
 
             // sub is normally fetched into pathtosub.srt
             console.log("Subtitle downloaded.");
