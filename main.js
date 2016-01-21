2c3e2a0f4e8a2499099deb1850b0b64e72cb19ec
pingpouff <pingpouff@gmail.com>
2016-01-21 23:12:09 +0100

    Added directory path argument feature.

--- main.js
+++ main.js
@@ -3,28 +3,49 @@ var fs = require("fs");
 var path = require("path");
 
 var subdb = new SubDb();
-var path_to_movie = process.argv[2];
-console.log("Hashing file at: " + path_to_movie);
-subdb.computeHash(path_to_movie, function (err, res) {
-    if (err) {
-        console.log(err);
-        return err;
-    }
-    console.log("Hash done.");
-    var hash = res;
-    console.log("Searching...");
-    subdb.api.search_subtitles(hash, function (err, res) {
+var pathArgument = process.argv[2];
 
-        if (err) return err;
-        console.log("Search done.");
-        var subFileName = path.dirname(path.resolve(path_to_movie)) + "/" + path.basename(path_to_movie, path.extname(path_to_movie)) + '.srt';
-        console.log("Downloading file for: " + subFileName);
-        subdb.api.download_subtitle(hash, res.join(','), subFileName, function (err, res) {
+var getSubFileFor = function (filePath) {
+    console.log("Hashing file at: " + filePath);
+    subdb.computeHash(filePath, function (err, res) {
+        if (err) {
+            console.log(err);
+            return err;
+        }
+        console.log("Hash done.");
+        var hash = res;
+        console.log("Searching...");
+        subdb.api.search_subtitles(hash, function (err, res) {
+
             if (err) return err;
+            console.log("Search done.");
+            var subFileName = path.dirname(path.resolve(filePath)) + "/" + path.basename(filePath, path.extname(filePath)) + '.srt';
+            console.log("Downloading file for: " + subFileName);
+            subdb.api.download_subtitle(hash, res.join(','), subFileName, function (err, res) {
+                if (err) return err;
 
-            // sub is normally fetched into pathtosub.srt
-            console.log("Subtitle downloaded.");
+                // sub is normally fetched into pathtosub.srt
+                console.log("Subtitle downloaded.");
+            });
+
         });
-
     });
-});
+};
+
+// Manage directory param
+if (fs.lstatSync(pathArgument).isDirectory()) {
+    console.log("Directory found!!!");
+    // TODO use async call + promise
+    fs.readdirSync(pathArgument).forEach(function (file) {
+        var fileExt = path.extname(file);
+        if (fileExt !== '.srt' && fileExt !== '.txt') {
+            var filePath = pathArgument + file;
+            console.log('Call for: ' + filePath);
+            getSubFileFor(filePath);
+        }
+        //console.log(fileExt);
+    });
+} else {
+    getSubFileFor(pathArgument);
+}
+
